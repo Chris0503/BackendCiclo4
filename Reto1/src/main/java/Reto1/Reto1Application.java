d8d6b9be24932e74308be69c0316bf4d440de6bc
Chris0503 <91760743+Chris0503@users.noreply.github.com>
2021-11-26 00:26:36 -0500

    Add files via upload

---
+++ Reto1/src/main/java/Reto1/Reto1Application.java
@@ -0,0 +1,19 @@
+//1.Modelo
+//2.Interface
+//3.Ropositorio
+//4.Servicio
+//5.COntrolador
+
+package Reto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto1Application.class, args);
+	}
+
+}
